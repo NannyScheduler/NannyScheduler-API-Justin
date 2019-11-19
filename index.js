cd0af780d323e65e672437a293a59ebb501e5ee1
mogwai <feelirabs@gmail.com>
2019-11-19 06:57:32 +0100

    Checked routes and server.js. Everything works.

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ const server = require('./server.js');
 
 const port = process.env.PORT || 9000
 
-server.get('/', (req, res) => {
-    res.status(201).send('<img src="https://media.giphy.com/media/UqUJhrD0om73q/giphy.gif"/>')
-});
-
 server.listen(port, ()=>console.log(`The server is alive on ${port}!`));
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const nannyRouter = require('./api/nannyRoute');
-const parentRouter = require('./api/parentRoute');
+// const parentRouter = require('./api/parentRoute');
 
 const server = express();
 
 //Routes
+server.get('/', (req, res) => {
+    res.status(201).send('<img src="https://media.giphy.com/media/1hBWHsBYoqYOfsmAsL/giphy.gif"/>')
+});
+
+server.get('/api', (req, res) => {
+    res.status(201).send('<img src="https://media.giphy.com/media/UqUJhrD0om73q/giphy.gif"/>')
+});
+
 server.use('/api/nannies', nannyRouter);
-server.use('api/parents', parentRouter);
+// server.use('api/parents', parentRouter);
 
 module.exports = server;
